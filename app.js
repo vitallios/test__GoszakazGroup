0470a8f7c21a976bc211360c47fc292414481aea
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-11-18 14:45:52 +0300

    add post list category

--- app.js
+++ app.js
@@ -5,35 +5,6 @@ btnMenu.addEventListener("click", () => {
   btnMenu.classList.toggle("active");
 });
 
-// const attrCategory = document.querySelectorAll('[data-category]');
-
-// attrCategory.forEach(item => {
-//  item.addEventListener('click', (event) => {
-//   // поск по номеру категории
-//   const categoryNamber = event.target.dataset.category;
-//   if (categoryNamber === 'Category_1') {
-//     location.href = '/category__pages/';
-//   } else {
-//     location.href = '404.html';
-//   }
-
-//  });
-// });
-// // поск по id поста
-// const idPost = document.querySelectorAll('.post__item');
-// idPost.forEach(item => {
-//  item.addEventListener('click', (event) => {
-//   const idPost = event.target.offsetParent.id;
-//   console.dir(idPost);
-
-//   if (idPost === 'post-0') {
-//     location.href = '/post_pages/';
-//   } else {
-//     location.href = '404.html';
-//   }
-
-//  })
-// });
 
 window.addEventListener("click", (e) => {
  const postElement = e.target
